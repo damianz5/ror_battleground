96e3f23ae763d7b16947b3e0199ff5726219d7d1
Damian Zabawa <damian.zabawa@ez.no>
2018-05-29 23:47:20 +0200

    Solutions

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,30 @@
-class CeasarCipher
+class CaesarCipher
+  attr_accessor :text
+  attr_accessor :num
 
+  def initialize(text, num=13)
+    @text = text
+    @num = num
+  end
+
+  def perform
+    caesar_cipher(@text, @num)
+  end
+
+  def caesar_cipher(string, shift = 1)
+    alphabet  = Array('a'..'z')
+    non_caps  = Hash[alphabet.zip(alphabet.rotate(shift))]
+
+    alphabet = Array('A'..'Z')
+    caps     = Hash[alphabet.zip(alphabet.rotate(shift))]
+
+    encrypter = non_caps.merge(caps)
+
+    string.chars.map { |c| encrypter.fetch(c, c) }.join
+  end
 end
+
+
+# cc = CaesarCipher.new('Ala ma...psa!', 4)
+#
+# puts cc.perform
--- passed_or_failed.rb
+++ passed_or_failed.rb
@@ -1,3 +1,24 @@
 class PassedOrFailed
+  def initialize(hash, num)
+    @hash = hash
+    @num = num.to_i
+  end
 
+  def result
+    passed, failed = {}, {}
+
+    @hash.each do |person, number|
+      number = number.to_i
+
+      if number >= @num
+        passed.store(person, number)
+      else
+        failed.store(person, number)
+      end
+    end
+
+    {:passed => passed, :failed => failed}
+  end
+
+  alias perform result
 end
